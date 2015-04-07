1b71161be22c4493e050108bca5d18eab9c881bb
Pascal <pascal@pascal.com>
2015-04-07 00:51:14 -0400

    setup demo of watch issue

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          livereload: false,
+          port: 9090,
+          hostname: '*',
+        }
+      }
+    },
+
+    watch: {
+      reload: {
+        files: [
+          '**/*.html',
+          '**/*.css',
+        ],
+        options: {
+          livereload: {
+            port: 9091,
+          },
+        },
+      },
+    },
+
+    concurrent: {
+      stack: {
+        tasks: [
+          'connect',
+          'watch:reload'
+        ],
+        options: {
+          logConcurrentOutput: true,
+          limit: 20
+        }
+      },
+    }
+  });
+
+  // define the default task
+  grunt.registerTask('default', ['concurrent:stack']);
+
+
+};
